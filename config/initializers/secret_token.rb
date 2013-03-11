49a9317803c4d05b6b2d6ba3b959b6616c2a225c
Timur Kaplya <timur@timur-Inspiron-N4050.(none)>
2013-03-11 21:57:16 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Oospey::Application.config.secret_token = '7642f6931fd2ccef6d0e6413b8cbb2f682bddc8d5848447411d20e7acde6f96bb3c205afdc4081377d42aa97f9cb324b81e6b0db6b5c73d2b85c983c1566da00'
