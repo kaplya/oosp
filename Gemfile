018af9bc3050363d84c3d48a824d5a0e41bfa607
Timur Kaplya <timur@timur-Inspiron-N4050.(none)>
2013-03-16 22:26:47 +0400

    add bootstrap

--- Gemfile
+++ Gemfile
@@ -26,6 +26,9 @@ gem 'jquery-rails'
 # To use ActiveModel has_secure_password
 gem 'bcrypt-ruby', '~> 3.0.0', :require => "bcrypt"
 
+gem 'bootstrap-sass'
+
+gem 'simple_form'
 # To use Jbuilder templates for JSON
 # gem 'jbuilder'
 
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Oospey::Application.routes.draw do
   get "sign_out" => "sessions#destroy", :as => "sign_out"
   get "sign_in" => "sessions#new", :as => "sign_in"
   get "sign_up" => "users#new", :as => "sign_up"
-  root :to => "users#new"
+  root :to => "sessions#new"
   resources :users
   resources :sessions
 
